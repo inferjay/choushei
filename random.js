22beb70d43f561e82b59407adf8110485a975641
inferjay <inferjay@gmail.com>
2016-11-27 19:08:45 +0800

    format code style

--- random.js
+++ random.js
@@ -1,42 +1,43 @@
 $(function() {
     var run = 0,
-    who, heading = $("h1"),
-    timer,
-    position = 0;
+        who, heading = $("h1"),
+        timer,
+        position = 0;
     var list = $("#list").val().replace(/ +/g, " ").replace(/^ | $/g, "").split(" ");
     $("#start").click(function() {
         if (!run) {
             heading.html(heading.html().replace("就是他！", "抽谁？"));
             $(this).val("停止");
             timer = setInterval(function() {
-                var r = Math.ceil(Math.random() * list.length);
-                who = list[r - 1];
-                $("#what").html(who);
-                var rTop = Math.ceil(Math.random() * $(document).height()),
-                rLeft = Math.ceil(Math.random() * ($(document).width() - 50)),
-                rSize = Math.ceil(Math.random() * (37 - 14) + 14);
-                $("<span class='temp'></span>").html(who).hide().css({
-                    "top": rTop,
-                    "left": rLeft,
-                    "color": "rgba(0,0,0,." + Math.random() + ")",
-                    "fontSize": rSize + "px"
-                }).appendTo("body").fadeIn("slow",
-                function() {
-                    $(this).fadeOut("slow",
-                    function() {
-                        $(this).remove();
-                    });
-                });
-            },
-            50);
+                    var r = Math.ceil(Math.random() * list.length);
+                    who = list[r - 1];
+                    $("#what").html(who);
+                    var rTop = Math.ceil(Math.random() * $(document).height()),
+                        rLeft = Math.ceil(Math.random() * ($(document).width() - 50)),
+                        rSize = Math.ceil(Math.random() * (37 - 14) + 14);
+                    $("<span class='temp'></span>").html(who).hide().css({
+                        "top": rTop,
+                        "left": rLeft,
+                        "color": "rgba(0,0,0,." + Math.random() + ")",
+                        "fontSize": rSize + "px"
+                    }).appendTo("body").fadeIn("slow",
+                        function() {
+                            $(this).fadeOut("slow",
+                                function() {
+                                    $(this).remove();
+                                });
+                        });
+                },
+                50);
             run = 1;
-        } else {++position;
+        } else {
+            ++position;
             $.each(list,
-            function(index, item) {
-                if (item == who) {
-                    list.splice(index, 1);
-                }
-            });
+                function(index, item) {
+                    if (item == who) {
+                        list.splice(index, 1);
+                    }
+                });
             if (typeof(who) != 'undefined') {
                 $("#prize").append("<p>").append(position + ":" + who).append("</p>");
             }
